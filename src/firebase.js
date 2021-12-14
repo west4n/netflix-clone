b151e054783d116377bd53071452505c2968c4d0
Ivan Kurski <iskurskiii@gmail.com>
2021-12-14 23:48:19 +0300

    INIT: Project init

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA8RsWOsZUK8bzU6s8K4uYNGiMEI3No52E",
+  authDomain: "netflix-clone-ec449.firebaseapp.com",
+  projectId: "netflix-clone-ec449",
+  storageBucket: "netflix-clone-ec449.appspot.com",
+  messagingSenderId: "818997007987",
+  appId: "1:818997007987:web:6ed9662bdf1a4db9ce3c06"
+};
