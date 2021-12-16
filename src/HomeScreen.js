efd1ef6cc2f1f9321f27aa4c4cc03b04cb9c4b62
west4n <iskurskiii@gmail.com>
2021-12-16 17:36:22 +0300

    FIX: fixing posters arrays

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -13,17 +13,17 @@ function HomeScreen() {
       <Nav />
       <Banner />
       <Row 
-        title='NETFLIX ORIGINALS'
+        title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
       />
-      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
-      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} /> 
-      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} /> 
-      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} /> 
-      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   )
 }
